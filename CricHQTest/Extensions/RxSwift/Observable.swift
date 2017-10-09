830ce20e866700dc483874279d6d6d7327df8a93
Blair McArthur <blair.mcarthur@skipthedishes.com>
2017-10-09 19:26:40 +1300

    deleted an unused file

--- CricHQTest/Extensions/RxSwift/Observable.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import RxSwift
-
-internal extension Observable where E: Sequence {
-    internal func mapElements<R>(_ transform: @escaping (E.Element) throws -> R) -> Observable<[R]> {
-        return map { try $0.map(transform) }
-    }
-}
